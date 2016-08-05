5dbc5cd17ac32cf469532e17368b401a28567d23
Zack Weinberg <zackw@panix.com>
2016-08-05 09:05:44 -0400

    Add Gaussian ranging function.

--- lib/ageo/ranging.py
+++ lib/ageo/ranging.py
@@ -12,7 +12,7 @@ from shapely.geometry import Point
 from shapely.ops import transform as sh_transform
 from functools import partial
 from sys import stderr
-from scipy import interpolate
+from scipy import interpolate, stats
 
 from .calibration import PhysicalLimitsOnly
 
@@ -98,3 +98,58 @@ class MinMax(RangingFunction):
 
     def unnormalized_pvals(self, dist):
         return self.interpolant(dist)
+
+class Gaussian(RangingFunction):
+    """A Gaussian ranging function is simply the pdf of a normal
+       distribution with mean and standard deviation given by the
+       calibration.
+
+       Right now it only makes sense to use ranging.Gaussian with
+       calibration.Spotter, and this class has intimate knowledge
+       of calibration.Spotter's internals.
+
+       For the reasons discussed above, the outer distance bound for
+       this function is the PHYSICAL_BOUNDS distance bound, and this
+       is also used as a "clip" on the pdf (which is nonzero
+       everywhere).
+    """
+
+    def __init__(self, *args, **kwargs):
+        RangingFunction.__init__(self, *args, **kwargs)
+        if not hasattr(self.calibration, '_mu') \
+           or not hasattr(self.calibration, '_sigma'):
+            raise TypeError("Gaussian ranging function requires a "
+                            "calibration with _mu and _sigma")
+        min_cal, max_cal = \
+            self.calibration.distance_range(self.rtts)
+        min_phy, max_phy = PHYSICAL_BOUNDS.distance_range(self.rtts)
+
+        self._distance_bound = max(min_cal, max_cal, min_phy, max_phy)
+
+        # FIXME: this logic belongs in calibration.Spotter.
+        med_rtt = np.percentile(self.rtts, .25)
+        mu = self.calibration._mu(med_rtt)
+        sigma = self.calibration._sigma(med_rtt)
+        # mu and sigma can go negative when two nodes are very close together,
+        # which causes stats.norm.pdf() to spit out nothing but NaN.
+        mu = max(mu, 1000)         # lower bound at 1km
+        sigma = max(sigma, 1000/3) # lower bound at 3sigma=1km
+        self._distribution = stats.norm(
+            loc   = mu,
+            scale = sigma
+        )
+
+    def distance_bound(self):
+        return self._distance_bound
+
+    def unnormalized_pvals(self, dist):
+        rv = self._distribution.pdf(dist)
+        if not np.isfinite(rv).all():
+            ve = ValueError("pdf returned non-finite values")
+            ve.req_domain = dist
+            ve.req_range = rv
+            ve.range_fn = self
+            raise ve
+
+        rv[dist > self._distance_bound] = 0
+        return rv

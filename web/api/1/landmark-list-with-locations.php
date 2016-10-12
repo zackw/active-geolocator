3c0dcd931846458fefd8e079a3ef5037f104d951
Zack Weinberg <zackw@panix.com>
2016-10-12 16:50:06 -0400

    The new web-based client

--- web/api/1/landmark-list-with-locations.php
+++ web/api/1/landmark-list-with-locations.php
@@ -13,28 +13,31 @@ include("config.php");
 $fp = fopen($LANDMARKS, "r");
 $data = [];
 while (($row = fgetcsv($fp)) !== FALSE) {
-    $data[] = [$row[0], $row[1] + 0, $row[2] + 0, $row[3] + 0];
+    $data[] = [$row[0], $row[1] + 0,
+               $row[2] + 0, $row[3] + 0,
+               $row[4] + 0, $row[5] + 0];
 }
 fclose($fp);
 
 # In addition, we ask the client to ping 127.0.0.1, its apparent
 # external IP address, a guess at its gateway address (last component
-# of the IPv4 address forced to .1) and this server.  We use TCP port 7
-# (echo protocol) for these, precisely because nobody runs an echo
-# responder anymore.
-$data[] = ["127.0.0.1", 7, 0, 0];
+# of the IPv4 address forced to .1) and this server.  We use TCP port
+# 80 for these partially because that's consistent with the main landmarks
+# list and partially because the JavaScript client isn't allowed to
+# connect to port 7.
+$data[] = ["127.0.0.1", 80, 0, 0, 0, 0];
 
 $client = $_SERVER["REMOTE_ADDR"];
 # in the unlikely event of an ipv6 address, don't bother; the client
 # can't handle them
 if (preg_match("/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/", $client)) {
     $gw = preg_replace("/\.[0-9]{1,3}$/", ".1", $client);
-    $data[] = [$client, 7, 0, 0];
+    $data[] = [$client, 80, 0, 0, 0, 0];
     if ($client !== $gw) {
-        $data[] = [$gw, 7, 0, 0];
+        $data[] = [$gw, 80, 0, 0, 0, 0];
     }
 }
-$data[] = [gethostbyname($_SERVER["HTTP_HOST"]), 7, 0, 0];
+$data[] = [gethostbyname($_SERVER["HTTP_HOST"]), 80, 0, 0, 0, 0];
 
 $blob = json_encode($data);
 if ($blob === FALSE) {
